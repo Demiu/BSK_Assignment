24afbfeb4dc0c9f0a57ff4c93097613ebb137665
Demiu <miazgapow@gmail.com>
2022-03-12 11:46:02 +0100

    Basic connecting loop in server

---
+++ lib/Server.cs
@@ -0,0 +1,41 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace lib;
+
+public class Server : IDisposable
+{
+    TcpListener listener;
+    CancellationTokenSource cancelTokenSource;
+
+    public Server(IPEndPoint endPoint) {
+        listener = new TcpListener(endPoint);
+        cancelTokenSource = new CancellationTokenSource();
+    }
+
+    ~Server() {
+        Dispose();
+    }
+
+    public void Dispose()
+    {
+        cancelTokenSource.Cancel();
+    }
+
+    public async Task ListenLoop() {
+        try {
+            var token = cancelTokenSource.Token;
+            listener.Start();
+            while (!token.IsCancellationRequested) {
+                var client = await listener.AcceptTcpClientAsync(cancelTokenSource.Token);
+                await HandleClient(client);
+            }
+        } finally {
+            listener.Stop();
+        }
+    }
+
+    private async Task HandleClient(TcpClient client) {
+        // TODO
+    }
+}
--- lib/TcpServer.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Net.Sockets;
-
-namespace lib;
-
-public class TcpServer
-{
-    
-}

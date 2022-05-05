0eaa16eb2fb624541ce1cb8262153ca4f83dd84a
grudos <jg04@wp.pl>
2022-05-05 11:54:10 +0200

    Encrypt for public and private key with hash and saving it, generating session key, divide file into packages

---
+++ lib/files/IFileManager.cs
@@ -0,0 +1,9 @@
+﻿namespace KeyStore.files;
+
+public interface IFileManager
+{
+    void SaveFile(byte[] data, string fileName);
+    byte[] GetFile(string fileName);
+    void DeleteFile(string fileName);
+    void DeleteAllFiles();
+}
